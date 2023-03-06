e32b8bdfc2188d04018a21f9eaaf8a046c6db55a
Anastasia Shaynyuk <anastasia_52669@ukr.net>
2023-03-05 22:53:36 -0700

    ice cream done

--- app.js
+++ app.js
@@ -19,10 +19,16 @@ const toppings = [{
   name: 'Sprinkles',
   image: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Sprinkles2.jpg',
   price: 1,
+  quantity: 0
 }, {
   name: 'Chocolate Chips',
   image: 'https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/chocolate-chips.jpg?quality=82&strip=1&resize=640%2C360',
   price: 2,
+  quantity: 0
+}, {
+  name: 'Gummy Worms',
+  price: 1,
+  quantity: 0
 }]
 
 const containers = [{
@@ -38,27 +44,32 @@ const containers = [{
 }]
 
 // console.log('is it workin?');
+let iceCreamTotal = 0
+let toppingsTotal = 0
+let waffleConsTotal = 0
 
 function addContainers(name) {
-  const waffleCons = containers.find(ves => ves.name == name)
-  containers.quantity++
-  console.log(waffleCons.name);
+  const waffleCons = containers.find(w => w.name == name)
+  waffleCons.quantity++
+  waffleConsTotal += waffleCons.price
+  console.log(waffleCons.name, waffleConsTotal);
 
   drawCardWaffle()
+  calculateTotal()
 }
 
 function drawCardWaffle() {
   // console.log('add waffles');
   let total = 0
   let template = ''
-  containers.forEach(ves => {
-    total += ves.price * ves.quantity
-  console.log('total waffles', total)  
-  //   if (ves.quantity) {
-  //     template += `
-  //     <div class="col-5">${ves.name}</div><div class="col-2">${ves.quantity}</div><div class="col-2">$${ves.price.toFixed(2)}</div><div class="col-3 text-center">$${(ves.quantity * ves.price).toFixed(2)}</div>
-  //     `
-  //   }
+  containers.forEach(w => {
+    total += w.price * w.quantity
+  // console.log('total waffles', total)  
+    if (w.quantity) {
+      template += `
+      <div class="col-5">${w.name}</div><div class="col-2">${w.quantity}</div><div class="col-2">$${w.price.toFixed(2)}</div><div class="col-3 text-center">$${(w.quantity * w.price).toFixed(2)}</div>
+      `
+    }
   })
   
   document.getElementById('waffles').innerHTML = template
@@ -69,9 +80,11 @@ function addIceCreamScoop(name2) {
   // console.log('adding ice cream', name);
   const iceCreamScoop = iceCream.find(ice => ice.name == name2)
 iceCreamScoop.quantity++
+iceCreamTotal += iceCreamScoop.price
 console.log(iceCreamScoop.name);
 
 drawCard()
+calculateTotal()
 }
 
 function drawCard() {
@@ -90,4 +103,46 @@ function drawCard() {
   // console.log('total', total);
 }
 
+function addToppings(name) {
+  const differentToppings = toppings.find(top => top.name == name)
+  differentToppings.quantity++
+  toppingsTotal += differentToppings.price
+  console.log(differentToppings.name);
+  drawToppings()
+  calculateTotal()
+}
+
+function drawToppings() {
+  let total =0
+  let template = ""
+  toppings.forEach(top => {
+    total += top.price * top.quantity
+    if (top.quantity){
+      template += `
+      <div class="col-5">${top.name}</div><div class="col-2">${top.quantity}</div><div class="col-2">$${top.price.toFixed(2)}</div><div class="col-3 text-center">$${(top.quantity * top.price).toFixed(2)}</div>
+      `
+    }
+  })
+  document.getElementById('toppings').innerHTML = template
+}
+
+
+function calculateTotal() {
+  const totalPrice = iceCreamTotal + toppingsTotal + waffleConsTotal;
+  document.getElementById('total').innerHTML = `$${totalPrice.toFixed(2)}`;
+    console.log(totalPrice, 'total');
+    return totalPrice;
+  }
+  
+
+function pay(){
+  let buy = calculateTotal()
+
+  if (buy > 0){
+    window.alert('Are you ready for checkout?')
+  } else {
+    window.alert('Your card is empty!')
+  }
+  
+}
 
